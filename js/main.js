6e37852bce58e4ad64cab53507ab6ce117406b6a
Jean Hubert ABA'A <78347379+jhabaa@users.noreply.github.com>
2023-04-30 23:50:50 +0200

    last version
    Suppression des fichiers inutiles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-//import * as THREE from  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
-//import { GLTFLoader } from "https://cdnjs.cloudflare.com/ajax/libs/three@0.152.0/examples/jsm/loaders/GLTFLoader.js";
-
 var scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -25,16 +22,52 @@ var contact = document.getElementById("contact");
 var items = document.querySelectorAll(".item");
 
 
+//Si sur mobile, on charge la scene verticale
+if (window.innerWidth < window.innerHeight) {
+    var scn_file = '/js/back3m.glb';
+}
+else {
+    var scn_file = '/js/back3.glb';
+}
 
-// Chargez la texture
-const textureLoader = new THREE.TextureLoader();
 
 var loader = new THREE.GLTFLoader();
-loader.load("/js/back3.glb", function (gltf) {
+loader.load(scn_file, function (gltf) {
     var o = gltf.scene;
     const camera = gltf.cameras[0];
+    //get first object in my 3d scene
+    const sun = gltf.scene.children[2];
+    sun.position.set(0, 0, 0);
+    
+    var currentLookAt = new THREE.Vector3(0, 0, 0);
 
-   
+    const light = gltf.scene.children[5];
+    const light_init_pos = light.position;
+    console.log(light);
+
+    const heanlab = gltf.scene.children[6];
+    const heanlab_init_pos = heanlab.position;
+    console.log(heanlab);
+    currentLookAt = heanlab_init_pos;
+
+    const dessert = gltf.scene.children[4];
+    const dessert_init_pos = dessert.position;
+    console.log(dessert);
+
+    const salad = gltf.scene.children[3];
+    const salad_init_pos = salad.position;
+    console.log(salad);
+
+    const burger = gltf.scene.children[9];
+    const burger_init_pos = burger.position;
+    console.log(burger);
+
+    const cameraCenter = new THREE.Vector3(0, 0, 0);
+    const cameraHorzLimit = 2;
+    const cameraVertLimit = 2;
+
+    o.remove(burger);
+
     //move camera on z axis
     camera.position.z += 0.5;
     const clip = gltf.animations[0];
@@ -46,7 +79,6 @@ loader.load("/js/back3.glb", function (gltf) {
     action.timeScale = 0.05;
 
 
-    action.play();
 
     const clock = new THREE.Clock();
     function animate() {
@@ -60,13 +92,13 @@ loader.load("/js/back3.glb", function (gltf) {
     }
     // Mettez à jour la fonction updateCamera pour déplacer la caméra en fonction de la position de la souris
     function updateCamera() {
-        const cameraCenter = new THREE.Vector3(0, 0, 0);
-        const cameraHorzLimit = 5;
-        const cameraVertLimit = 5;
+        
 
         camera.position.x = cameraCenter.x + (cameraHorzLimit * mouse.x);
+
+        //get an object in my 3d scene
        camera.position.y = cameraCenter.y + (cameraVertLimit * mouse.y);
-       camera.lookAt(cameraCenter); // Assurez-vous que la caméra regarde toujours le centre de la scène
+       camera.lookAt(currentLookAt); // Assurez-vous que la caméra regarde toujours l'object en focus
     }
     animate()
 
@@ -74,12 +106,6 @@ loader.load("/js/back3.glb", function (gltf) {
 });
 
 const light = new THREE.AmbientLight(0xffffff, 1)
-
-//scene.add(light);
-
-
-//set up mouse stuff
-//document.addEventListener('mousemove', onDocumentMouseMove, false);
 
 
 // Mettez à jour la fonction onDocumentMouseMove pour normaliser les coordonnées de la souris
@@ -97,19 +123,6 @@ function onWindowResize() {
 }
 
 
-//buttons actions
-var btn = document.getElementById("back_btn");
-btn.addEventListener("click", () => {
-    items.forEach(item => {
-        if(item.classList.contains("current")) {
-            console.log("current");
-            item.classList.toggle("current");
-        }
-        //item.classList.add("hide");
-    })
-});
-
-
 //Action des divers elements
 items.forEach(item => {
     item.addEventListener("click", () => {
@@ -118,8 +131,6 @@ items.forEach(item => {
         });
         item.classList.remove("hide");
         item.classList.toggle("current");
-
-        
     })
 });
 
